b36ad8e6df230004b0426b586372d39a7350d7fa
MASKO <markus-kornmann@outlook.com>
2024-12-04 18:58:55 +0100

    app.test mit jest

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ function App() {
   const [currentTrack, setCurrentTrack] = useState(0);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const tracks = [
-    { src: "https://www.amigaremix.com/listen/3611/solarstriker_-_lotus_2_o.stelzer_remix_-_amigaremix_03611.mp3", title: "Lotus 2" },
-    { src: "https://www.amigaremix.com/listen/3612/gareth_wood_-_spaceballs_-_state_of_the_art_no_corruption_mix_-_amigaremix_03612.mp3", title: "Spaceballs" },
-    { src: "https://www.amigaremix.com/listen/3548/tony_fluke73_wiren_-_turrican_ii_freedom_live_performance_-_amigaremix_03548.mp3", title: "Turrican2" },
-    { src: "https://www.amigaremix.com/listen/3545/dr.future_-_apidya_highscore_-_amigaremix_03545.mp3", title: "Apidya" },
-    { src: "https://www.amigaremix.com/listen/2805/mortalshock_-_giana_sisters_intro_remix_-_amigaremix_02805.mp3", title: "Giana Sisters" },
-    { src: "https://www.amigaremix.com/listen/52/deadly_cookie_-_monkey_island_-_amigaremix_00052.mp3", title: "Monkey Island" },
-    { src: "https://www.amigaremix.com/listen/2888/mano_-_wing_commander_-_intro_-_amigaremix_02888.mp3", title: "Wing Commander" },
-    { src: "https://www.amigaremix.com/listen/3062/pinozulpo_-_speedball_2_-_brutal_antanux_-_amigaremix_03062.mp3", title: "Speedball2" },
-    { src: "https://www.amigaremix.com/listen/2173/d.a.wilson_-_crystal_hammer_-_electropops_mix_-_amigaremix_02173.mp3", title: "Crystal Hammer" },
-    { src: "https://www.amigaremix.com/listen/112/infamous_-_kid_chaos_running_red_remix_-_amigaremix_00112.mp3", title: "Kid Chaos" },
+    { src: "https://lmy.de/vgNHg", title: "Lotus 2" },
+    { src: "https://lmy.de/pgygn", title: "Spaceballs" },
+    { src: "https://lmy.de/wNTCD", title: "Turrican2" },
+    { src: "https://lmy.de/foZMo", title: "Apidya" },
+    { src: "https://lmy.de/AFBOr", title: "Giana Sisters" },
+    { src: "https://lmy.de/XpiLv", title: "Monkey Island" },
+    { src: "https://lmy.de/JcuJr", title: "Wing Commander" },
+    { src: "https://lmy.de/qvWeM", title: "Speedball2" },
+    { src: "https://lmy.de/aeluu", title: "Crystal Hammer" },
+    { src: "https://lmy.de/HauJG", title: "Kid Chaos" },
   ];
 
   useEffect(() => {
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App';
+
+test('sollte die Audio-Elemente und Playlist korrekt rendern', () => {
+    render(<App />);
+    const audioPlayer = screen.getByRole('audio');
+    const playlistItems = screen.getAllByRole('listitem');
+
+    expect(audioPlayer).toBeInTheDocument();
+    expect(playlistItems.length).toBe(10); // Anzahl der Tracks in der Playlist
+});
+
+test('sollte den ersten Track abspielen', () => {
+    render(<App />);
+    const audioPlayer = screen.getByRole('audio');
+    const firstTrack = screen.getByText('Lotus 2');
+
+    fireEvent.click(firstTrack);
+    expect(audioPlayer.src).toContain('https://www.amigaremix.com/listen/3611/solarstriker_-_lotus_2_o.stelzer_remix_-_amigaremix_03611.mp3');
+});
+
+test('sollte den nächsten Track abspielen, wenn der aktuelle Track endet', () => {
+    render(<App />);
+    const audioPlayer = screen.getByRole('audio');
+    const firstTrack = screen.getByText('Lotus 2');
+    const secondTrack = screen.getByText('Spaceballs');
+
+    fireEvent.click(firstTrack);
+    fireEvent.ended(audioPlayer);
+
+    expect(audioPlayer.src).toContain('https://www.amigaremix.com/listen/3612/gareth_wood_-_spaceballs_-_state_of_the_art_no_corruption_mix_-_amigaremix_03612.mp3');
+});
+
+/*import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -6,3 +41,4 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+*/
